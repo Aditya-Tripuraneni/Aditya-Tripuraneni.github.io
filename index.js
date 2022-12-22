8e986ef585ba76ad9450aeb02f954772d9edb96b
Aditya <tripuraa@mcmaster.ca>
2022-12-22 16:30:16 -0500

    mac eng p2 added

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-console.log("Hello World!")
-console.log('tESTING Features')
-
-
-
-
 isDark = true 
+
 function changeMode(){
     backgroundColor = document.getElementById("body")
+
+    headerTwo = document.getElementsByTagName("h2")
+
     headerThree = document.getElementsByTagName("h3")
     headerFive = document.getElementsByTagName("h5")
     paragraphs = document.getElementsByTagName("p")
     if (isDark == true)
     {
-        
-
-
         for (var i = 0; i < headerThree.length; i ++){
             headerThree[i].style.color = "black"
         }
@@ -27,7 +22,13 @@ function changeMode(){
             paragraphs[i].style.color = "black"
         }
 
+        for (var i = 0; i < headerTwo.length; i ++){
+            headerTwo[i].style.color = "black"
+        }
+
         backgroundColor.style.background = "white"
+        headerTwo[0].style.color = "black"
+
         isDark = false
     }
     else{
@@ -43,9 +44,13 @@ function changeMode(){
             paragraphs[i].style.color = "white"
         }
 
-        backgroundColor.style.background = "#BFB8CB"
-        isDark = true
+        for (var i = 0; i < paragraphs.length; i ++){
+            headerTwo[i].style.color = "white"
+        }
 
+        backgroundColor.style.background = "#BFB8CB"
+        headerTwo[0].style.color = "white"
+        isDark = true
     }
 
 }

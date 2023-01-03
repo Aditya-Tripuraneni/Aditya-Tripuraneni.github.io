e05622839580e999ed456dba7b3db80fb064682b
Aditya <tripuraa@mcmaster.ca>
2023-01-03 17:52:27 -0500

    animation bug fixed

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ webInfoEl = document.getElementById("web-info")
 
 headerTwo = document.getElementsByTagName("h2")
 
-headerThree = document.getElementsByTagName("h3")
+subTitleEl = document.getElementsByClassName("sub-title")
+timeLineNames = document.getElementsByClassName("time")
+console.log(timeLineNames)
+
+
 headerFour = document.getElementsByTagName("h4")
 paragraphs = document.getElementsByTagName("p")
 
@@ -84,6 +88,10 @@ function changeMode(){
         changeWebsiteBlack()
         backgroundColor.style.background = "white"
         backgroundColor.classList.add("trial")
+        backgroundColor.addEventListener("animationend", function(){
+            backgroundColor.classList.remove("trial")
+        })
+
         headerTwo[0].style.color = "black"
         isDark = false
     }
@@ -92,6 +100,9 @@ function changeMode(){
         changeWebsiteWhite()
         backgroundColor.style.background = "#2a2a2a"
         backgroundColor.classList.add("trial")
+        backgroundColor.addEventListener("animationend", function(){
+            backgroundColor.classList.remove("trial")
+        })
         headerTwo[0].style.color = "white"
         isDark = true
     }
@@ -100,37 +111,45 @@ function changeMode(){
 }
 
 function changeWebsiteWhite(){
-    for (var i = 0; i < headerThree.length; i ++){
-        headerThree[i].style.color = "white"
+    for (let i = 0; i < subTitleEl.length; i ++){
+        subTitleEl[i].style.color = "white"
     }
 
-    for (var i = 0; i < headerFour.length; i ++){
+    for (let i = 0; i < timeLineNames.length; i ++){
+        timeLineNames[i].style.color = "white"
+    }
+
+    for (let i = 0; i < headerFour.length; i ++){
         headerFour[i].style.color = "white"
     }
 
-    for (var i = 0; i < paragraphs.length; i ++){
+    for (let i = 0; i < paragraphs.length; i ++){
         paragraphs[i].style.color = "white"
     }
 
-    for (var i = 0; i < paragraphs.length; i ++){
+    for (let i = 0; i < paragraphs.length; i ++){
         headerTwo[i].style.color = "white"
     }
 }
 
 function changeWebsiteBlack(){
-    for (var i = 0; i < headerThree.length; i ++){
-        headerThree[i].style.color = "black"
+    for (let i = 0; i < subTitleEl.length; i ++){
+        subTitleEl[i].style.color = "black"
     }
 
-    for (var i = 0; i < headerFour.length; i ++){
+    for (let i = 0; i < timeLineNames.length; i ++){
+        timeLineNames[i].style.color = "black"
+    }
+
+    for (let i = 0; i < headerFour.length; i ++){
         headerFour[i].style.color = "black"
     }
 
-    for (var i = 0; i < paragraphs.length; i ++){
+    for (let i = 0; i < paragraphs.length; i ++){
         paragraphs[i].style.color = "black"
     }
 
-    for (var i = 0; i < headerTwo.length; i ++){
+    for (let i = 0; i < headerTwo.length; i ++){
         headerTwo[i].style.color = "black"
     }
 }
